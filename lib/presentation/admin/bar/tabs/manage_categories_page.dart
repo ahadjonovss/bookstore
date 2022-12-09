f73b4fce02819c15c2bf9a6d40aa961e21fc383a
Samandar <samandarahadjonov@gmail.com>
2022-12-09 10:17:22 +0500

    edit category done

--- lib/presentation/admin/bar/tabs/manage_categories_page.dart
+++ lib/presentation/admin/bar/tabs/manage_categories_page.dart
@@ -1,8 +1,7 @@
-import 'package:animated_snack_bar/animated_snack_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shopee/components/widgets/animated_snackbar.dart';
 import 'package:shopee/data/models/category_model.dart';
+import 'package:shopee/presentation/admin/bar/tabs/widgets/edit_category_dialog.dart';
 import 'package:shopee/view_models/admin/categories_view_model.dart';
 
 class ManageCategoriesPage extends StatelessWidget {
@@ -30,75 +29,14 @@ class ManageCategoriesPage extends StatelessWidget {
                         IconButton(
                             onPressed: () {
                               showDialog(context: context, builder: (context) {
-                                return  Dialog(
-                                  child: Container(
-                                    height: MediaQuery.of(context).size.height*0.245,
-                                    width: MediaQuery.of(context).size.width*0.8,
-                                    padding: const EdgeInsets.all(20),
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20)
-                                    ),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children:  [
-                                        const Text("Edit Category",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),),
-                                        SizedBox(height: MediaQuery.of(context).size.height*0.028,),
-                                        Column(
-                                          children: [
-                                        Container(
-                                          decoration: BoxDecoration(
-                                            borderRadius: BorderRadius.circular(20),
-                                            border: Border.all(color: Colors.black)
-                                          ),
-                                          padding: const EdgeInsets.only(left: 12),
-                                          child:  TextField(
-                                            controller: categoryController,
-                                            decoration:  InputDecoration(
-                                              hintText: category.categoryName,
-                                              border: InputBorder.none
-                                            ),
-                                          ),
-                                        ),
-                                        SizedBox(height: MediaQuery.of(context).size.height*0.02,),
-                                        Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            TextButton(onPressed: (){Navigator.pop(context);}, child: Text("Cancel",style: TextStyle(color: Colors.red,fontWeight: FontWeight.w600,fontSize: 20),)),
-                                            TextButton(onPressed: () async {
-                                              if(categoryController.text.length<5){
-                                                MySnackBar(context, notification: "Name of Category must include\ncharacters more than 5", color: Colors.red, icon: Icon(Icons.warning,color: Colors.white,));
-                                              }
-                                              else{
-                                                category.categoryName=categoryController.text;
-                                                await context.read<CategoriesViewModel>().updateCategory(category.docId!, category);
-                                                Navigator.pop(context);
-                                                MySnackBar(context, notification: "Name of category changed succesfully!", color: Colors.green, icon: Icon(Icons.info_rounded,color: Colors.white,));
-
-                                              }
-
-
-                                            }, child: Text("Save",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.w600,fontSize: 20),))
-                                          ],
-                                        )
-
-
-
-                                          ],
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                );
+                                return  EditCategoryDialog(category: category,categoryController: categoryController);
                               },
                               );
-
                             },
                             icon: const Icon(Icons.edit)),
                         IconButton(
                             onPressed: () async {
                               await context.read<CategoriesViewModel>().deleteByDocId(category.docId!);
-
-
                             },
                             icon: const Icon(Icons.delete)),
                       ],
@@ -112,9 +50,8 @@ class ManageCategoriesPage extends StatelessWidget {
             return Center(child: Text(snapshot.error.toString()),);
           }
           if(snapshot.connectionState==ConnectionState.waiting){
-            return Center(child: CircularProgressIndicator(),);
+            return const Center(child: CircularProgressIndicator(),);
           }
-
           return Container(
               height: MediaQuery.of(context).size.height,
               width: MediaQuery.of(context).size.width,
---
+++ lib/presentation/admin/bar/tabs/widgets/edit_category_dialog.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopee/components/widgets/animated_snackbar.dart';
+import 'package:shopee/data/models/category_model.dart';
+import 'package:shopee/view_models/admin/categories_view_model.dart';
+
+// ignore: must_be_immutable
+class EditCategoryDialog extends StatelessWidget {
+  TextEditingController categoryController;
+  CategoryModel category;
+  EditCategoryDialog({required this.category,required this.categoryController,Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        height: MediaQuery.of(context).size.height*0.245,
+        width: MediaQuery.of(context).size.width*0.8,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20)
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  [
+            const Text("Edit Category",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w600),),
+            SizedBox(height: MediaQuery.of(context).size.height*0.028,),
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.black)
+                  ),
+                  padding: const EdgeInsets.only(left: 12),
+                  child:  TextField(
+                    controller: categoryController,
+                    decoration:  InputDecoration(
+                        hintText: category.categoryName,
+                        border: InputBorder.none
+                    ),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height*0.02,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(onPressed: (){Navigator.pop(context);}, child: const  Text("Cancel",style: TextStyle(color: Colors.red,fontWeight: FontWeight.w600,fontSize: 20),)),
+                    TextButton(onPressed: () async {
+                      if(categoryController.text.length<5){
+                        MySnackBar(context, notification: "Name of Category must include\ncharacters more than 5", color: Colors.red, icon: const Icon(Icons.warning,color: Colors.white,));
+                      }
+                      else{
+                        category.categoryName=categoryController.text;
+                        await context.read<CategoriesViewModel>().updateCategory(category.docId!, category);
+                        // ignore: use_build_context_synchronously
+                        Navigator.pop(context);
+                        // ignore: use_build_context_synchronously
+                        MySnackBar(context, notification: "Name of category changed succesfully!", color: Colors.green, icon: const Icon(Icons.info_rounded,color: Colors.white,));
+                      }
+                    }, child: const Text("Save",style: TextStyle(color: Colors.blue,fontWeight: FontWeight.w600,fontSize: 20),))
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
