4c9d7b15c1f11414f1e0c8983c63c9d9c85149ac
Samandar <samandarahadjonov@gmail.com>
2022-12-11 20:56:56 +0500

    done: add image from web

--- lib/presentation/admin/bar/add_product_second.dart
+++ lib/presentation/admin/bar/add_product_second.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:shopee/data/models/book_model.dart';
+import 'package:shopee/data/services/file_uploader.dart';
 
 // ignore: must_be_immutable
 class AddImagesToProduct extends StatefulWidget {
@@ -11,6 +13,8 @@ class AddImagesToProduct extends StatefulWidget {
 }
 
 class _AddImagesToProductState extends State<AddImagesToProduct> {
+  final ImagePicker _picker = ImagePicker();
+  String imageUrl = "";
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   TextEditingController imgctrl = TextEditingController();
   bool isHasPhoto = false;
@@ -200,6 +204,10 @@ class _AddImagesToProductState extends State<AddImagesToProduct> {
                                         ),
                                       )
                                     : InkWell(
+                                  onTap: (){
+                                    _showPicker(context);
+                                    print(imageUrl);
+                                  },
                                         child: Container(
                                           height: 60,
                                           width: double.infinity,
@@ -273,4 +281,60 @@ class _AddImagesToProductState extends State<AddImagesToProduct> {
       ),
     );
   }
+
+
+void _showPicker(context) {
+  showModalBottomSheet(
+      context: context,
+      builder: (BuildContext bc) {
+        return SafeArea(
+          child: Wrap(
+            children: <Widget>[
+              ListTile(
+                  leading: const Icon(Icons.photo_library),
+                  title: const Text("Gallery"),
+                  onTap: () {
+                    _getFromGallery();
+                    Navigator.of(context).pop();
+                  }),
+              ListTile(
+                leading: const Icon(Icons.photo_camera),
+                title: const Text('Camera'),
+                onTap: () {
+                  _getFromCamera();
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      });
 }
+
+_getFromGallery() async {
+  XFile? pickedFile = await _picker.pickImage(
+    maxWidth: 1000,
+    maxHeight: 1000,
+    source: ImageSource.gallery,
+  );
+  if (pickedFile != null) {
+    if (!mounted) return;
+    imageUrl = await FileUploader.imageUploader(pickedFile);
+    setState(() {});
+  }
+}
+
+_getFromCamera() async {
+  XFile? pickedFile = await _picker.pickImage(
+    maxWidth: 1920,
+    maxHeight: 2000,
+    source: ImageSource.camera,
+  );
+  if (pickedFile != null) {
+    if (!mounted) return;
+    imageUrl = await FileUploader.imageUploader(pickedFile);
+    setState(() {});
+  }
+}
+}
+
